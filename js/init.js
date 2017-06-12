e13cefb61077d012260209d4a3db35dfbdba029e
willbenmitch <ben.mitchell01@gmail.com>
2017-06-12 15:23:27 -0400

    update style

--- js/init.js
+++ js/init.js
@@ -3,8 +3,7 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-    $('.carousel').carousel({duration: 300});
-    $('.carousel-slider').carousel({fullWidth: true});
+    $('.carousel').carousel({duration: 200});
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
