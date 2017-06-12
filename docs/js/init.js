e0b8eb3bfc89be61040bd3a7e9391efdcd2d6767
willbenmitch <ben.mitchell01@gmail.com>
2017-06-12 14:07:24 -0400

    init personal-site

---
+++ docs/js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.carousel').carousel({duration: 300});
+    $('.carousel-slider').carousel({fullWidth: true});
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
